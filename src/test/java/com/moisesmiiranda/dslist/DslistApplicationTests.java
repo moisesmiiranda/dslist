e282ea9d01c5a6b76d24c258c650a26350e51e7b
Moisés Miranda <moisesmiranda1994@gmail.com>
2023-05-09 22:56:13 -0300

    Project created

---
+++ src/test/java/com/moisesmiiranda/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moisesmiiranda.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
